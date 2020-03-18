d142021ef43713af9ccc907c8ed355b3603c7e8b
Jeremy J Starcher <jeremy.starcher@gmail.com>
2020-03-18 17:53:12 -0400

    Started to add a fantasy console in

---
+++ sources/systems/fantasy_console/machine.h
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <strings.h>
+#include <stdio.h>
+#include <stdint.h>
+
+//6502 CPU registers
+extern uint16_t pc;
+extern uint8_t sp, a, x, y, status;
+extern void reset6502(void);
+extern uint32_t clockticks6502;
+
+extern void step6502(void);
+
+void js_step6502(uint16_t *buf, int len);
+uint8_t read6502(uint16_t address);
+void write6502(uint16_t address, uint8_t value);
+void testrun();
