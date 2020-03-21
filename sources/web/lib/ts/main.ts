f384072db7e3dc24b906d38f41bf8b4b8b1c13d3
Jeremy J Starcher <jeremy.starcher@gmail.com>
2020-03-21 01:15:30 -0400

    Both bundles are made

--- sources/web/lib/ts/main.ts
+++ sources/web/lib/ts/main.ts
@@ -1,8 +1,7 @@
 declare const Module: any;
 
- import common from "./common";
+import common from "./common";
 
-//import common from "common.js";
 import route from "./route.js";
 import { editor } from "./editor.js";
 import { fileList } from "./filelist.js";
--- sources/web/rollup.config.js
+++ sources/web/rollup.config.js
@@ -1,13 +1,26 @@
 import typescript from 'rollup-plugin-typescript';
 
-export default {
-	input: './lib/ts/main.ts',
-	plugins: [
-		typescript()
-	],
-	output: {
-		sourcemap: "./lib/ts/bundle.js.map",
-		file: './lib/ts/bundle.js',
-		format: 'es'
+export default [
+	{
+		input: './lib/ts/main.ts',
+		plugins: [
+			typescript()
+		],
+		output: {
+			sourcemap: "./lib/ts/main-bundle.js.map",
+			file: './lib/ts/main-bundle.js',
+			format: 'es'
+		},
 	},
-}
+	{
+		input: './lib/ts/dasm.ts',
+		plugins: [
+			typescript()
+		],
+		output: {
+			sourcemap: "./lib/ts/dasm-bundle.js.map",
+			file: './lib/ts/dasm-bundle.js',
+			format: 'es'
+		},
+	},
+]
